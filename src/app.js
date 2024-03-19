9429b0282c5abceb06b01dc6f1bef19c33ab4e17
2746-sg <156344773+2746-sg@users.noreply.github.com>
2024-03-19 20:11:13 +0000

    cambio

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let who = ["Mi madre", "Mi amigo", "Mi vecino", "Mi perro"];
+let action = ["come", "corre", "grita", "bebe"];
+let what = ["mi comida", "muy rapido", "muy fuerte"];
+let when = [
+  "despues de trabajar",
+  "cuando tiene prisa",
+  "cuando se enfada",
+  "cuando tiene mucha sed"
+];
+let nuevaexcusa = `${who[Math.floor(Math.random() * who.length)]} ${
+  action[Math.floor(Math.random() * action.length)]
+} ${what[Math.floor(Math.random() * what.length)]} ${
+  when[Math.floor(Math.random() * when.length)]
+}`;
+window.onload = document.getElementById("nuevaexcusa").innerHTML = nuevaexcusa;
+
